0816269905c68b4ff07e2ba52fcd60d9933f9072
BernardoMB <bmondragonbrozon@gmail.com>
2019-04-04 16:54:48 -0600

    Added login by token route

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+require('dotenv-flow').config({
+  node_env: process.env.NODE_ENV || 'development'
+});
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -10,11 +14,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
 
-app.get('/ping', (req, res, next)  => {
+app.get('/ping', (req, res, next) => {
+  console.log('Get /ping ');
   res.status(200).json('pong!');
 });
 
-app.post('/register', (req, res, next)  => {
+app.post('/register', (req, res, next) => {
+  console.log('POST /register ');
   if (req.body.email === 'test@test.com') {
     res.status(201).json({
       status: 'success',
@@ -28,6 +34,7 @@ app.post('/register', (req, res, next)  => {
 });
 
 app.post('/login', (req, res, next) => {
+  console.log('POST /login ');
   if (req.body.email === 'test@test.com') {
     res.status(200).json({
       status: 'success',
@@ -40,7 +47,26 @@ app.post('/login', (req, res, next) => {
   }
 });
 
-app.get('/status', (req, res, next)  => {
+app.post('/login-with-token', (req, res, next) => {
+  console.log('POST /login-with-token ');
+  const token = req.body.token;
+  const isValidToken = true;
+  setTimeout(() => {
+    if (isValidToken) {
+      res.status(200).json({
+        email: 'test@test.com',
+        token: '1234567'
+      });
+    } else {
+      res.status(401).json({
+        status: 'error'
+      });
+    }
+  }, 0);
+});
+
+app.get('/status', (req, res, next) => {
+  console.log('GET /status ');
   if (!(req.headers && req.headers.authorization)) {
     return res.status(400).json({
       status: 'error'
@@ -48,7 +74,7 @@ app.get('/status', (req, res, next)  => {
   }
   // simulate token decoding
   const header = req.headers.authorization.split(' ');
-  const token = header[1];
+  const token = header[ 1 ];
   if (token === '1234567') {
     res.status(200).json({
       status: 'success',
